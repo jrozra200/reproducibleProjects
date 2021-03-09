9c499497ae7d707b53f3c637ab009630830e9d12
rozran00@gmail.com <rozran00@gmail.com>
2021-03-09 13:46:34 -0500

    trying something new

--- R/hello_word.R
+++ R/hello_word.R
@@ -10,5 +10,12 @@ hello_world <- function(path, ...) {
 
     # ensure path exists
     dir.create(path, recursive = TRUE, showWarnings = FALSE)
+    dir.create(paste0(path, "/R"), recursive = TRUE, showWarnings = FALSE)
+    dir.create(paste0(path, "/data"), recursive = TRUE, showWarnings = FALSE)
+    dir.create(paste0(path, "/viz"), recursive = TRUE, showWarnings = FALSE)
+    dir.create(paste0(path, "/output"), recursive = TRUE, showWarnings = FALSE)
+
+
+    #final_analysis.Rmd, R/make_data.R, R/explore_data.Rmd, data/external/, data/raw/, data/interim/, data/processed/, viz/, output/
 
 }
