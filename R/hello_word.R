8fd28ee44a0e6e41b5dc04be7908f7149501c31a
rozran00@gmail.com <rozran00@gmail.com>
2021-03-11 10:50:05 -0500

    do I even need the path?

--- R/hello_word.R
+++ R/hello_word.R
@@ -10,10 +10,10 @@
 hello_world <- function(path) {
 
     # CREATE THE FILE STRUCTURE FOR THE ANALYSIS
-    source(paste0(path, "/R/create_directories.R"))
+    # source(paste0(path, "/R/create_directories.R"))
     create_directories(path)
 
     # CREATE THE FINAL ANALYSIS FILE
-    source(paste0(path, "/R/build_final_analysis_rmd.R"))
+    # source(paste0(path, "/R/build_final_analysis_rmd.R"))
     build_final_analysis_rmd(path)
 }
