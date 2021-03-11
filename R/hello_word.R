1d3db52315a64494c360203fec6c16382425de39
rozran00@gmail.com <rozran00@gmail.com>
2021-03-11 10:29:51 -0500

    use new scripts/functions

--- R/hello_word.R
+++ R/hello_word.R
@@ -9,48 +9,11 @@
 #' hello_world()
 hello_world <- function(path) {
 
-    # CREATE THE PARENT DIRECTORY; ENSURE THAT PATH EXISTS
-    dir.create(path, recursive = TRUE, showWarnings = FALSE)
-
-    # CREATE THE R DIRECTORY; ENSURE THAT PATH EXISTS
-    dir.create(paste0(path, "/R"), recursive = TRUE, showWarnings = FALSE)
-
-    # CREATE THE DATA DIRECTORY & SUBDIRECTORIES; ENSURE THAT PATH EXISTS
-    dir.create(paste0(path, "/data"), recursive = TRUE, showWarnings = FALSE)
-    dir.create(paste0(path, "/data/external"), recursive = TRUE, showWarnings = FALSE)
-    dir.create(paste0(path, "/data/raw"), recursive = TRUE, showWarnings = FALSE)
-    dir.create(paste0(path, "/data/interim"), recursive = TRUE, showWarnings = FALSE)
-    dir.create(paste0(path, "/data/processed"), recursive = TRUE, showWarnings = FALSE)
-
-    # CREATE THE VISUALIZATON DIRECTORY; ENSURE THAT PATH EXISTS
-    dir.create(paste0(path, "/visualization"), recursive = TRUE, showWarnings = FALSE)
-
-    # CREATE THE OUTPUT DIRECTORY; ENSURE THAT PATH EXISTS
-    dir.create(paste0(path, "/output"), recursive = TRUE, showWarnings = FALSE)
+    # CREATE THE FILE STRUCTURE FOR THE ANALYSIS
+    source("R/create_directories.R")
+    create_directories(path)
 
     # CREATE THE FINAL ANALYSIS FILE
-
-    # CREATE THE HEADER FOR FINAL ANALYSIS FILE
-    header <- c("---", "title: \"Untitled\"", "author: \"AUTHOR\"",
-                "date: \"DATE\"", "output: html_document", "---\n")
-
-    # CREATE THE DATA CHUNK FOR FINAL ANALYSIS FILE
-    chunk <- c("```{r setup, include = FALSE}", "# SETUP CHUNK FOR EXAMPLE",
-               "```\n")
-
-    # CREATE THE BODY FOR FINAL ANALYSIS FILE
-    body <- c("## R Markdown\n", "This is where your final analysis should go")
-
-    # COMBINE THE HEADER, DATA CHUNK, AND BODY INTO A SINGLE STRING
-    contents <- paste(
-        paste(header, collapse = "\n"),
-        paste(chunk, collapse = "\n"),
-        paste(body, collapse = "\n"),
-        sep = "\n")
-
-    # STREAM STRING INTO THE FINAL ANALYSIS FILE
-    writeLines(contents, con = file.path(path, "final_analysis.Rmd"))
-
-    #R/make_data.R, R/explore_data.Rmd
-
+    source("R/build_final_analysis_rmd.R")
+    build_final_analysis_rmd(path)
 }
