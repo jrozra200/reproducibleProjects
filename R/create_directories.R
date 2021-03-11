c0925895b1314105a5b6667995d594324ecc8fb9
rozran00@gmail.com <rozran00@gmail.com>
2021-03-11 10:27:51 -0500

    moving the creation of file structure into a standalone script

--- R/create_directories.R
+++ R/create_directories.R
@@ -1,1 +1,30 @@
-# CREATE THE DIRECTORIES HERE
+#' Create the necessary file structure
+#'
+#' This function creates the necessary file structure for your analysis.
+#'
+#' @param path pass a path to the function
+#' @keywords
+#' @export
+#' @examples
+#' create_directories()
+create_directories <- function(path) {
+
+    # CREATE THE PARENT DIRECTORY; ENSURE THAT PATH EXISTS
+    dir.create(path, recursive = TRUE, showWarnings = FALSE)
+
+    # CREATE THE R DIRECTORY; ENSURE THAT PATH EXISTS
+    dir.create(paste0(path, "/R"), recursive = TRUE, showWarnings = FALSE)
+
+    # CREATE THE DATA DIRECTORY & SUBDIRECTORIES; ENSURE THAT PATH EXISTS
+    dir.create(paste0(path, "/data"), recursive = TRUE, showWarnings = FALSE)
+    dir.create(paste0(path, "/data/external"), recursive = TRUE, showWarnings = FALSE)
+    dir.create(paste0(path, "/data/raw"), recursive = TRUE, showWarnings = FALSE)
+    dir.create(paste0(path, "/data/interim"), recursive = TRUE, showWarnings = FALSE)
+    dir.create(paste0(path, "/data/processed"), recursive = TRUE, showWarnings = FALSE)
+
+    # CREATE THE VISUALIZATON DIRECTORY; ENSURE THAT PATH EXISTS
+    dir.create(paste0(path, "/visualization"), recursive = TRUE, showWarnings = FALSE)
+
+    # CREATE THE OUTPUT DIRECTORY; ENSURE THAT PATH EXISTS
+    dir.create(paste0(path, "/output"), recursive = TRUE, showWarnings = FALSE)
+}
